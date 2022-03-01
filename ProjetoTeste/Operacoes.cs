cf2c8ea6c352f5a90c6e6ac08a6f836133d52973
Arthur M <arthurmauro1989@gmail.com>
2022-03-01 08:43:14 -0300

    Primeiro Commit teste de Unidade

---
+++ ProjetoTeste/Operacoes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ProjetoTeste
+{
+    public class Operacoes
+    {
+        public static double Somar(double pNum, double sNum)
+        {
+            return (pNum + sNum);
+        }
+
+    }
+}
